d8171904eed5137bda94b2fa13ecb1e8304ebb58
Urvish Rupareliya <rupareliyaurvish@gmail.com>
2024-09-06 18:59:01 +0530

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,20 @@ import { FRONT_END_DOMAIN } from "@/constant/ogConstant";
 // import { FRONT_END_DOMAIN } from "@/constant";
 import React from "react";
 
-export async function getServerSideProps(context) {
-    const ogImageUrl = `${FRONT_END_DOMAIN}/api/og/home`;
-    return {
-        props: {
-            ogData: {
-                url: `${FRONT_END_DOMAIN}/`,
-                imgUrl: ogImageUrl,
-                title: "TesseractX",
-                description:
-                    "TesseractX is the ultimate rewarding, multi-chain, community-centric digital collectibles marketplace.",
-            },
-        },
-    };
-}
+// export async function getServerSideProps(context) {
+//     const ogImageUrl = `${FRONT_END_DOMAIN}/api/og/home`;
+//     return {
+//         props: {
+//             ogData: {
+//                 url: `${FRONT_END_DOMAIN}/`,
+//                 imgUrl: ogImageUrl,
+//                 title: "TesseractX",
+//                 description:
+//                     "TesseractX is the ultimate rewarding, multi-chain, community-centric digital collectibles marketplace.",
+//             },
+//         },
+//     };
+// }
 
 const index = () => {
     return (
